5b801acfd5b9e04f2398e211c129b8c412ef361f
KavyanshKhaitan2 <kavyanshkhaitan11@gmail.com>
2025-09-19 14:37:39 +0530

    Added description to the definitions at 'To Implement'

--- pyautogui/_pyautogui_wayland.py
+++ pyautogui/_pyautogui_wayland.py
@@ -1,18 +1,17 @@
 """
 To Implement:
-[x] _position
-[ ] _size
-[ ] _vscroll
-[ ] _hscroll
-[ ] _scroll
-[ ] _click
-[ ] _mouse_is_swapped
-[ ] _moveTo
-[ ] _mouseDown
-[ ] _mouseUp
-[ ] _keyDown
-[ ] _keyUp
-[ ] 
+[x] _position: Return xy coordinates of mouse
+[ ] _size: Return screen size
+[ ] _vscroll: Vertical mouse scroll
+[ ] _hscroll: Horizontal mouse scroll
+[ ] _scroll: Mouse scroll
+[ ] _click: Mouse click
+[ ] _mouse_is_swapped: Check if LR mouse buttons are swapped.
+[ ] _moveTo: Moves the mouse cursor to a point on the screen
+[ ] _mouseDown: Mouse Down
+[ ] _mouseUp: Mouse Up (if mouse is down)
+[ ] _keyDown: Key Down
+[ ] _keyUp: Key Up (if key is down)
 """
 
 import pyautogui
@@ -22,13 +21,14 @@ import subprocess
 from pyautogui import LEFT, MIDDLE, RIGHT
 
 import wayland_automation
+import pyscreenshot
 
 def _position():
     """Returns the current xy coordinates of the mouse cursor as a two-integer
     tuple.
 
     Returns:
-      (x, y) tuple of the current xy coordinates of the mouse cursor.
+        (x, y) tuple of the current xy coordinates of the mouse cursor.
     """
     gen = wayland_automation.mouse_position_generator()
     pos = next(gen)
